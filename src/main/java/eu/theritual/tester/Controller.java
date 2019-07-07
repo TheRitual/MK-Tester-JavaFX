1cf59ebe3b781ea30ce82f5b29542225104a1959
TheRitual <dev@theritual.eu>
2019-07-07 21:41:53 +0200

    Created Basic Event

--- src/main/java/eu/theritual/tester/Controller.java
+++ src/main/java/eu/theritual/tester/Controller.java
@@ -1,4 +1,14 @@
 package eu.theritual.tester;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+    @FXML Button butt;
+
+    @FXML
+    public void onButtonClick() {
+        System.out.println(butt.getText());
+    }
+
 }
